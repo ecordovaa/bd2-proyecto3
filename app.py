1c09ab5513adbdaf337f9a156cf633fc5d753a4f
jZtrK <efrain.cordova@utec.edu.pe>
2021-08-06 15:05:46 -0500

    added rtree.data

--- app.py
+++ app.py
@@ -46,5 +46,5 @@ def upload_image():
     '''
 
 if __name__ == "__main__":
-    create_rtree_index()
+    create_rtree_index(50)
     app.run(host='0.0.0.0', port=8080, debug=True)
--- rtree_index.py
+++ rtree_index.py
@@ -1,5 +1,6 @@
 import face_recognition as fr
 from rtree import index
+import numpy as np
 from os import listdir
 from sklearn.decomposition import PCA
 
@@ -8,25 +9,19 @@ def encode_file(file):
     res = fr.face_encodings(img)
     if(len(res) > 0):
         return res[0]
-    return [0] * 49
+    return [0] * 128
 
-def create_rtree_index(n = 13234):
+def create_rtree_index(n):
     p = index.Property()
     p.dimension = 128
     p.dat_extension = 'data'
     p.idx_extension = 'index'
     idx = index.Index('rtree_index', properties=p)
-    n_components = 49
-    encodings = []
-    pca = PCA(n_components=n_components, whiten=True)
-    i = 0
+    encodings = np.empty(shape=2)
     for img in listdir('temp/'):
-        print(img)
-        i += 1
-        encodings.append(encode_file(f'temp/{img}'))
-    i = 0
-    pca.fit(encodings)
-    for enc in encodings:
-        idx.insert(i, tuple(enc))
-        i+=1
+        encodings += encode_file(f'temp/{img}').reshape(-1,1)
+    p = PCA(n_components=1, whiten=True)
+    p.fit_transform(encodings)
+    for i in range(n):
+        idx.insert(i, tuple(encodings[i]))
     idx.close()
